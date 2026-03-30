75bd3db9b1cc6de5a8a7d2f65ef0599fa436deb4
Abhey2003i <abheyjohary@gmail.com>
2026-03-30 13:10:03 +0530

    updated portfolio changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,9 @@
 export const projects=[
 
 {
-title:"React Ecommerce",
-desc:"Shopping platform using Redux",
-github:"#"
+title:"hospital CRM",
+desc:"Hospital CRM & Management System is a full-stack web application built using React.js and Firebase. It streamlines hospital operations by providing role-based dashboards for Admin, Doctor, and Patient, ensuring efficient management of appointments, prescriptions, and patient records. The Admin dashboard allows for user management and system oversight, while the Doctor dashboard facilitates appointment scheduling and prescription management. The Patient dashboard offers a user-friendly interface for booking appointments and accessing medical records. This project demonstrates proficiency in React.js for frontend development and Firebase for backend services, showcasing a comprehensive solution for healthcare management.",
+github:"https://github.com/Abhey2003/hospital-crm"
 },
 
 {
@@ -14,7 +14,7 @@ github:"#"
 
 {
 title:"Developer Portfolio",
-desc:"A modern frontend developer portfolio built with React and TailwindCSS, featuring animations, project showcase, certifications, and a recruiter-friendly interface with resume download.",
+desc:"Developed a responsive and interactive portfolio website using React and TailwindCSS, featuring smooth animations, project showcase, certifications section, and a recruiter-friendly interface with easy navigation and resume download functionality.",
 github:"https://github.com/Abhey2003/Abhey_portfolio/commit/8380f567193352beb91e602ce81767608c51c869#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5"
 }
 
